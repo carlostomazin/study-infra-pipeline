bdd9b185facb760f587cab201cdd384d527e60de
carlostomazin <carlos.tomazin2@gmail.com>
2024-06-10 20:43:07 -0300

    teste auto pr

--- infra/main.tf
+++ infra/main.tf
@@ -3,24 +3,6 @@ resource "aws_s3_bucket" "bucket" {
     force_destroy = true
 }
 
-resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
-  bucket = aws_s3_bucket.bucket.id
-  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
-}
-
-data "aws_iam_policy_document" "allow_access_from_another_account" {
-  statement {
-
-    actions = [
-      "s3:GetObject",
-    ]
-
-    resources = [
-      "${aws_s3_bucket.bucket.arn}/*",
-    ]
-  }
-}
-
 resource "aws_s3_bucket_website_configuration" "blog" {
   bucket = aws_s3_bucket.bucket.id
   index_document {
