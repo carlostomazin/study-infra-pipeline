a58e7ed930470dd98c4e188f5cbf82b2bd2c3239
carlostomazin <carlos.tomazin2@gmail.com>
2024-06-10 23:11:07 -0300

    teste s3

--- infra/s3.tf
+++ infra/s3.tf
@@ -27,23 +27,6 @@ resource "aws_s3_bucket_policy" "this" {
   })
 }
 
-resource "aws_s3_bucket_website_configuration" "blog" {
-  bucket = aws_s3_bucket.bucket.id
-  index_document {
-    suffix = "index.html"
-  }
-  error_document {
-    key = "error.html"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "public_access_block" {
-  bucket = aws_s3_bucket.bucket.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
 
 ##### will upload all the files present under HTML folder to the S3 bucket #####
 resource "aws_s3_object" "upload_object" {
