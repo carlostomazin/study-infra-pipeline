00a9551bc4c97d0d4e4db1a02bbc8f43a1184075
carlostomazin <carlos.tomazin2@gmail.com>
2024-06-10 23:11:25 -0300

    teste s3

--- infra/s3.tf
+++ infra/s3.tf
@@ -26,14 +26,3 @@ resource "aws_s3_bucket_policy" "this" {
     ],
   })
 }
-
-
-##### will upload all the files present under HTML folder to the S3 bucket #####
-resource "aws_s3_object" "upload_object" {
-  for_each      = fileset("html/", "*")
-  bucket        = aws_s3_bucket.bucket.id
-  key           = each.value
-  source        = "html/${each.value}"
-  etag          = filemd5("html/${each.value}")
-  content_type  = "text/html"
-}
